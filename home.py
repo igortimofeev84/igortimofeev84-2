394522be94f34e77878eee94fb6c2443ab3d6096
igortimofeev84 <igor_timofeev84@mail.ru>
2022-04-28 14:01:42 +0300

    myproject

---
+++ home.py
@@ -0,0 +1,18 @@
+[]############### Добавление комментария ########################
+# import time
+# from selenium import webdriver
+# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+# driver.maximize_window()
+# driver.implicitly_wait(10)
+# driver.get("http://practice.automationtesting.in/")
+# driver.execute_script("window.scrollBy(0, 600);")
+# button = driver.find_element_by_css_selector(".text-22-sub_row_1-0-2-0-0 :nth-child(2)").click()
+# REVIEWS = driver.find_element_by_class_name("reviews_tab").click()
+# fivestars= driver.find_element_by_class_name("star-5").click()
+# comment = driver.find_element_by_css_selector("[name=comment]")
+# comment.send_keys("Nice book!")
+# Name= driver.find_element_by_id("author")
+# Name.send_keys("Aleksandr")
+# email= driver.find_element_by_id("email")
+# email.send_keys("123g@mail.ru")
+# submit= driver.find_element_by_id("submit").click()
